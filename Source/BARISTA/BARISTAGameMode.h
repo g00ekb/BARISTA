f761d350385535dadab9a048d9784119033ca31a
tgmf <2.tagmf@gmail.com>
2025-04-01 17:16:13 +0700

    proper .gitignore

---
+++ Source/BARISTA/BARISTAGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BARISTAGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABARISTAGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABARISTAGameMode();
+};
+
+
+
